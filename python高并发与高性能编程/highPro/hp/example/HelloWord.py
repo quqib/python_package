262c485dca8ac130a12425a6803da6b22dea7544
zhaoyang <1058873515@qq.com>
2026-03-11 17:34:31 +0800

    关于python库的学习整理归纳-多线程

--- python高并发与高性能编程/highPro/hp/example/HelloWord.py
+++ python高并发与高性能编程/highPro/hp/example/HelloWord.py
@@ -2,6 +2,7 @@
 Time: 2026年03月10日 11:56:40
 Author: Written by Mr.zhaoyang
 """
+from threading import Thread
 
 """
 
@@ -32,7 +33,26 @@ python线程⑦个状态：
     线程阻塞状态
     线程结束状态
 Cpython虚拟机中，多线程是通过切换线程上下文进行熟悉爱你的，线程切换会造成资源开销
+    线程锁用来保护线程的安全
     
 """
 
+import threading
+import time
 
+# 存在GIL时的线程开销
+def readFile():
+    print("线程:" + str(threading.current_thread().name) + "开始执行...")
+    start = time.time()
+    with open("file.txt", "r") as f:
+        data = f.read()
+        print(data)
+    end = time.time()
+    print(f"线程:{threading.current_thread().name}执行完成，耗时:{end - start:.2f}秒")
+
+
+t1 = Thread(target=readFile)
+t2 = Thread(target=readFile)
+
+t1.start()
+t2.start()
